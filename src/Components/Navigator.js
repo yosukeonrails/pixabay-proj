701c96deddaac7f6ef8b85e47c4eaeaae1ae19f6
Yoh <yosuke.hishinuma@dfinsolutions.com>
2020-03-25 13:08:06 -0700

    added buffer to input search

--- src/Components/Navigator.js
+++ src/Components/Navigator.js
@@ -3,54 +3,56 @@ import KeywordSearch from "./KeywordSearch";
 import CategoryDropdown from "./CategoryDropdown";
 
 const Navigator = props => {
-  const buffer = 2000;
+    const buffer = 200;
+    let querySearchBufferTimer;
 
-  const sendSearch = query => {
-    console.log("SENT REQUEST FOR :" + query);
-    props.dispatchCurrentSearchParams({ type: "QUERY", query: query });
-    props.submitSearch({ ...props.currentSeachParams, query: query });
-  };
+    const sendSearch = query => {
+        props.dispatchCurrentSearchParams({ type: "QUERY", query: query });
+        props.submitSearch({ ...props.currentSeachParams, query: query });
+    };
 
-  const querySearchBufferTimer = query => {
-    setTimeout(() => {
-      sendSearch(query);
-    }, buffer);
-  };
+    const toggleSearch = query => {
+        clearBuffer();
+        querySearchBufferTimer =
+            setTimeout((query) => {
+                console.log(query);
+                sendSearch(query);
+            }, buffer, query);
+    };
 
-  const toggleSearch = query => {
-    clearTimeout(querySearchBufferTimer);
-    querySearchBufferTimer(query);
-  };
+    const clearBuffer = () => {
+        clearTimeout(querySearchBufferTimer);
+    }
 
-  const toggleCategory = category => {
-    props.dispatchCurrentSearchParams({ type: "CATEGORY", category: category });
-    props.submitSearch({
-      ...props.currentSeachParams,
-      category: category,
-      page: 1
-    });
-  };
+    const toggleCategory = category => {
+        props.dispatchCurrentSearchParams({ type: "CATEGORY", category: category });
+        props.submitSearch({
+            ...props.currentSeachParams,
+            category: category,
+            page: 1
+        });
+    };
 
-  return (
-    <div className="navigator-container">
-      <div className="navigator centralized">
-        <KeywordSearch toggleSearch={toggleSearch} />
+    return (
+        <div className="navigator-container">
+            <div className="navigator centralized">
+                <KeywordSearch toggleSearch={toggleSearch} />
 
-        <CategoryDropdown setCategory={toggleCategory} />
+                <CategoryDropdown setCategory={toggleCategory} />
 
-        <button
-          onClick={() => {
-            // here we reset page to 1
-            props.dispatchCurrentSearchParams({ type: "PAGE", page: 1 });
-            props.submitSearch({ ...props.currentSeachParams, page: 1 });
-          }}
-          class="btn btn-primary"
-        >
-          Search
+                <button
+                    onClick={() => {
+                        // here we reset page to 1
+                        props.dispatchCurrentSearchParams({ type: "PAGE", page: 1 });
+                        props.submitSearch({ ...props.currentSeachParams, page: 1 });
+                    }}
+                    class="btn btn-primary"
+                >
+                    Search
         </button>
-      </div>
-    </div>
-  );
+            </div>
+        </div>
+    );
 };
 
 export default Navigator;
