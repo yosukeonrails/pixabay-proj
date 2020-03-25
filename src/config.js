7aa383a166e065c3f198b6142b0216fc175bc802
yosukeyoh <yosukeonrails@gmail.com>
2020-03-25 07:26:20 -0700

    added back api key to config

--- src/config.js
+++ src/config.js
@@ -1,3 +1,7 @@
 require("dotenv").config();
+
+// In this instance the "production" api key is the same as the testing one just for
+// demontration purposes. In reality one would be just for testing and vice-versa.
+
 exports.PIXABAY_API_KEY =
-  process.env.REACT_APP_PIXABAY_API_KEY || "test-api-key";
+  process.env.REACT_APP_PIXABAY_API_KEY || "13136421-266c28a6d61717bc2e4e6a83e";
