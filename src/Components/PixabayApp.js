4be804d862c21af80c68c2a6aa7b68b309d6f387
Yoh <yosuke.hishinuma@dfinsolutions.com>
2020-03-25 13:29:53 -0700

    added listener cleaner

--- src/Components/PixabayApp.js
+++ src/Components/PixabayApp.js
@@ -4,41 +4,46 @@ import "../Styles/PixabayApp.scss";
 import SavedLinksList from "./SavedLinksList";
 
 function PixabayApp() {
-  const [savedImages, setSavedImages] = useState({});
-  const [windowSize, setWindowSize] = useState(window.innerWidth);
-  const [savedLinkToggeled, setSavedLinkToggeled] = useState(false);
-  const handleResize = () => {
-    setWindowSize(window.innerWidth);
-  };
+    const [savedImages, setSavedImages] = useState({});
+    const [windowSize, setWindowSize] = useState(window.innerWidth);
+    const [savedLinkToggeled, setSavedLinkToggeled] = useState(false);
+    const handleResize = () => {
+        setWindowSize(window.innerWidth);
+    };
 
-  useEffect(() => {
-    window.addEventListener("resize", () => {
-      handleResize();
-    });
-  }, []);
+    useEffect(() => {
 
-  let showSaveLinkWindow = windowSize > 600 || savedLinkToggeled;
+        window.addEventListener("resize", () => {
+            handleResize();
+        });
 
-  return (
-    <div className="pixabay-app">
-      <ImageBrowser setSavedImages={setSavedImages} savedImages={savedImages} />
-      <div
-        style={{ display: showSaveLinkWindow ? "none" : "block" }}
-        className="saved-side-tag"
-        onClick={() => {
-          setSavedLinkToggeled(true);
-        }}
-      >
-        <p>{`( ${Object.keys(savedImages).length} )  Saved`} </p>
-      </div>
-      <SavedLinksList
-        showSaveLinkWindow={showSaveLinkWindow}
-        windowSize={windowSize}
-        savedImages={savedImages}
-        setSavedLinkToggeled={setSavedLinkToggeled}
-      />
-    </div>
-  );
+        return () => {
+            window.removeEventListener('resize', handleResize)
+        }
+    }, []);
+
+    let showSaveLinkWindow = windowSize > 600 || savedLinkToggeled;
+
+    return (
+        <div className="pixabay-app">
+            <ImageBrowser setSavedImages={setSavedImages} savedImages={savedImages} />
+            <div
+                style={{ display: showSaveLinkWindow ? "none" : "block" }}
+                className="saved-side-tag"
+                onClick={() => {
+                    setSavedLinkToggeled(true);
+                }}
+            >
+                <p>{`( ${Object.keys(savedImages).length} )  Saved`} </p>
+            </div>
+            <SavedLinksList
+                showSaveLinkWindow={showSaveLinkWindow}
+                windowSize={windowSize}
+                savedImages={savedImages}
+                setSavedLinkToggeled={setSavedLinkToggeled}
+            />
+        </div>
+    );
 }
 
 export default PixabayApp;
